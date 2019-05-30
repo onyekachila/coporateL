8831da0f2df3eebc80d41fbc686c83254cc2a5f8
Obaro <obaro.ogbo@gmail.com>
2019-05-30 10:53:37 +0100

    Initial commit

---
+++ database/migrations/2019_05_29_214508_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('status');
+            $table->integer('posted_by');
+            $table->text('body');
+            $table->string('image');
+            $table->integer('like');
+            $table->integer('dislike');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
